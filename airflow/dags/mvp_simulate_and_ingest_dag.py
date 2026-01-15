ddd9532f25bacce2cdf4ba52caa3bff20f046ffb
lewandowkox <you@example.com>
2026-01-15 23:17:41 +0100

    step 1 airflow done

--- airflow/dags/mvp_simulate_and_ingest_dag.py
+++ airflow/dags/mvp_simulate_and_ingest_dag.py
@@ -9,9 +9,10 @@ from docker.types import Mount
 
 DEFAULT_ARGS = {"owner": "airflow", "retries": 1, "retry_delay": timedelta(minutes=2)}
 
-# HOST_PROJECT_DIR dostajesz z docker-compose (PROJECT_DIR: ${PWD})
-HOST_PROJECT_DIR = os.environ.get("PROJECT_DIR", "/opt/airflow/jobs")
-APP_DIR = "/app"  # ścieżka wewnątrz kontenera python
+HOST_PROJECT_DIR = os.environ.get(
+    "PROJECT_DIR",
+    "/Users/olga/Desktop/WPBD_repo/mini-data-platform-lewandowkox"
+)
 
 with DAG(
     dag_id="mvp_simulate_and_ingest",
@@ -28,20 +29,27 @@ with DAG(
         image="python:3.11-slim",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
-        auto_remove=True,
-        command=f"bash -lc \"cd {APP_DIR} && python generate_csv.py\"",
-        mounts=[Mount(source=HOST_PROJECT_DIR, target=APP_DIR, type="bind")],
+        auto_remove="success",
+        mount_tmp_dir=False,
+        command=(
+            "bash -lc "
+            "'pip install -q pandas faker && python /app/generate_csv.py'"
+        ),
+        mounts=[Mount(source=HOST_PROJECT_DIR, target="/app", type="bind")],
     )
 
+
     ingest_to_postgres = DockerOperator(
-        task_id="ingest_to_postgres",
-        image="python:3.11-slim",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",
-        auto_remove=True,
-        # MVP: doinstaluj deps w locie (żeby działało bez budowania image)
-        command=f"bash -lc \"cd {APP_DIR} && pip install -q psycopg2-binary pandas && python csv_ingestion.py\"",
-        mounts=[Mount(source=HOST_PROJECT_DIR, target=APP_DIR, type="bind")],
-    )
+    task_id="ingest_to_postgres",
+    image="python:3.11-slim",
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge",
+    auto_remove="success",
+    mount_tmp_dir=False,
+    command="bash -lc 'pip install -q pandas sqlalchemy psycopg2-binary && python /app/csv_ingestion.py'",
+    mounts=[Mount(source=HOST_PROJECT_DIR, target="/app", type="bind")],
+)
+
+
 
     generate_csv >> ingest_to_postgres
